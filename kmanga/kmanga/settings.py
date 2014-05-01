2e709ef33773e19db347cfdca86b6722a178f825
Alberto Planas <aplanas@suse.de>
2014-05-01 13:03:33 +0200

    Downgrade Django to 1.6.4

--- kmanga/kmanga/settings.py
+++ kmanga/kmanga/settings.py
@@ -2,10 +2,10 @@
 Django settings for kmanga project.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/dev/topics/settings/
+https://docs.djangoproject.com/en/1.6/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/dev/ref/settings/
+https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -14,10 +14,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')_p674z@f%0j!8^$#66t^hje+w+b@&m01*-!q^(yn*+35w%xyn'
+SECRET_KEY = 'k#b0wj-@8zg(a!pwdk-%$g1qf&#z%&hfzi9#ubi(#&6((-%%_^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -43,7 +43,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -54,7 +53,7 @@ WSGI_APPLICATION = 'kmanga.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/dev/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -64,7 +63,7 @@ DATABASES = {
 }
 
 # Internationalization
-# https://docs.djangoproject.com/en/dev/topics/i18n/
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -78,6 +77,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/dev/howto/static-files/
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
--- kmanga/kmanga/urls.py
+++ kmanga/kmanga/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import patterns, include, url
 
-urlpatterns = [
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'kmanga.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-]
+)
