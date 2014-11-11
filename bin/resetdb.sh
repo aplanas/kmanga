4df8c608306462d003648b400e2b7a31b6fc318f
Alberto Planas <aplanas@gmail.com>
2014-11-11 12:50:57 +0100

    Make resetdb.sh configurable.

---
+++ bin/resetdb.sh
@@ -0,0 +1,17 @@
+# !/bin/sh
+
+# Load the configuration file if exist
+if [ -f "etc/config.sh" ]; then
+    . ./config.sh
+fi
+
+if [ -z "$PYTHON" ]; then
+    PYTHON=python
+fi
+
+rm kmanga/db.sqlite3
+rm kmanga/main/migrations/000*
+$PYTHON kmanga/manage.py makemigrations
+$PYTHON kmanga/manage.py migrate
+$PYTHON kmanga/manage.py createsuperuser --username aplanas --email aplanas@gmail.com
+$PYTHON kmanga/manage.py loaddata kmanga/initialdata.json
--- kmanga/resetdb.sh
+++
@@ -1,8 +0,0 @@
-# !/bin/sh
-
-rm db.sqlite3
-rm main/migrations/000*
-./manage.py makemigrations
-./manage.py migrate
-./manage.py createsuperuser --username aplanas --email aplanas@gmail.com
-./manage.py loaddata initialdata.json
