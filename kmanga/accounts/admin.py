e870acb6c4895bdd0bebd41be73fb40099d7c318
Alberto Planas <aplanas@suse.de>
2014-11-27 06:40:07 +0100

    Add basic user profile in the admin.

--- kmanga/accounts/admin.py
+++ kmanga/accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from accounts.models import Profile
 
 # Register your models here.
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class UserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- kmanga/accounts/models.py
+++ kmanga/accounts/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    FREE = 'F'
+    PAY = 'P'
+    MODE_CHOICES = (
+        (FREE, 'Non-pay user'),
+        (PAY, 'Pay user'),
+    )
+    GERMAN = 'DE'
+    ENGLISH = 'EN'
+    SPANISH = 'ES'
+    FRENCH = 'FR'
+    ITALIAN = 'IT'
+    RUSSIAN = 'RU'
+    LANGUAGE_CHOICES = (
+        (ENGLISH, 'English'),
+        (SPANISH, 'Spanish'),
+        (GERMAN, 'German'),
+        (FRENCH, 'French'),
+        (ITALIAN, 'Italian'),
+        (RUSSIAN, 'Russian'),
+    )
+
+    user = models.OneToOneField(User)
+    mode = models.CharField(max_length=1, choices=MODE_CHOICES,
+                            default=FREE)
+    language = models.CharField(max_length=2, choices=LANGUAGE_CHOICES)
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, **kwargs):
+    
