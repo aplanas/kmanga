09fffed8311265e147ce7f6bf88a29ed7beeb9ec
Alberto Planas <aplanas@gmail.com>
2015-06-15 21:59:28 +0200

    Add test coverage.

--- bin/test.sh
+++ bin/test.sh
@@ -7,10 +7,47 @@ if [ ! -f "bin/kmanga.conf" ]; then
 fi
 source bin/kmanga.conf
 
+
+function usage {
+    echo "Wrapper to test KManga."
+    echo
+    echo "$0 [-h | -c]"
+    echo "  -h  for help"
+    echo "  -c  to run the test with coverage.py"
+    echo
+}
+
+
 error_code=0
+run_coverage=0
+
+while [ "$1" != "" ]; do
+    case $1 in
+	-c)
+	    run_coverage=1
+	    ;;
+ 
+	-h | --help)
+	    usage $0
+	    exit 0
+	    ;;
+	*)
+	    usage $0
+	    exit 0
+	    ;;
+    esac
+    shift
+done
 
 # External tests
-DJANGO_SETTINGS_MODULE=kmanga.settings python -m unittest discover -s ./tests
+if [ $run_coverage -eq 0 ]; then
+    DJANGO_SETTINGS_MODULE=kmanga.settings \
+			  python -m unittest discover -s ./tests
+else
+    DJANGO_SETTINGS_MODULE=kmanga.settings \
+			  coverage run --branch -m --source=mobi,scraper \
+			  unittest discover -s ./tests
+fi
 if [ $? -ne 0 ]; then
     echo "Error in external tests"
     error_code=1
@@ -18,7 +55,13 @@ fi
 
 # Django tests
 cd kmanga
-./manage.py test
+if [ $run_coverage -eq 0 ]; then
+    ./manage.py test
+else
+    mv ../.coverage .
+    coverage run -a --branch --source='.' manage.py test
+    mv .coverage ../
+fi
 if [ $? -ne 0 ]; then
     echo "Error in django tests"
     error_code=1
