57fbbff074bd37017c65258d4603135ab32c1a14
Alberto Planas <aplanas@gmail.com>
2015-11-30 16:11:51 +0100

    Add 502 as a retry error.

--- scraper/scraper/settings.py
+++ scraper/scraper/settings.py
@@ -92,7 +92,7 @@ import os.path
 # Retry many times since proxies often fail
 RETRY_TIMES = 10
 # Retry on most error codes since proxies fail for different reasons
-RETRY_HTTP_CODES = [500, 503, 504, 400, 403, 404, 408]
+RETRY_HTTP_CODES = [500, 502, 503, 504, 400, 403, 404, 408]
 
 # Some proxy generate redirects of other errors, this codes invalidate
 # the proxy and is mapped as a RETRY_HTTP_CODE 500
