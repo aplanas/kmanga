ecbb56cca8248dfd8f62f91ed8b65a6d24df687b
Alberto Planas <aplanas@gmail.com>
2015-12-08 16:26:54 +0100

    Use more generic import path.

--- kmanga/core/views.py
+++ kmanga/core/views.py
@@ -3,13 +3,13 @@ from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponseForbidden
 from django.http import HttpResponseRedirect
 # from django.shortcuts import render
+from django.views.generic import CreateView
+from django.views.generic import DeleteView
 from django.views.generic import DetailView
+from django.views.generic import FormView
 from django.views.generic import ListView
-from django.views.generic.base import TemplateView
-from django.views.generic.edit import CreateView
-from django.views.generic.edit import DeleteView
-from django.views.generic.edit import FormView
-from django.views.generic.edit import UpdateView
+from django.views.generic import TemplateView
+from django.views.generic import UpdateView
 from django.views.generic.list import MultipleObjectMixin
 
 from .forms import ContactForm
