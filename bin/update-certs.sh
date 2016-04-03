f6d9c6ed5d0d1e53e776acc5e6523e4ccea2ca47
Alberto Planas <aplanas@gmail.com>
2016-04-03 21:41:12 +0200

    update-certs: update intermediate.pem certificate.

--- bin/update-certs.sh
+++ bin/update-certs.sh
@@ -13,7 +13,7 @@ python $ACME_PATH/acme_tiny.py \
        --csr $CERTS_PATH/domain.csr \
        --acme-dir $CHALLENGE_PATH > $CERTS_PATH/signed.crt.NEW || exit
 mv $CERTS_PATH/signed.crt.NEW $CERTS_PATH/signed.crt
-wget -O - https://letsencrypt.org/certs/lets-encrypt-x1-cross-signed.pem > $CERTS_PATH/intermediate.pem
+wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > $CERTS_PATH/intermediate.pem
 cat $CERTS_PATH/signed.crt $CERTS_PATH/intermediate.pem > $CERTS_PATH/chained.pem
 
 # Add in /etc/sudoers.d/httpd a line like:
