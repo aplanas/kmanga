991bc7405fe0b01c5e646acff67594ebc23fbf16
Alberto Planas <aplanas@gmail.com>
2016-06-11 22:09:55 +0200

    clean-objects: add issues and decrease the time.

--- bin/clean-objects.sh
+++ bin/clean-objects.sh
@@ -10,29 +10,35 @@ fi
 . $VENV/bin/activate
 
 # Clean mangas and list next candidates
-DAYS=20
-LOG=$LOG_PATH/candidates-clean-manga-$(date "+%Y-%m-%d-%T").log
-kmanga/manage.py clean manga --list -d $DAYS &> $LOG
-bin/check-log.sh $LOG
+# DAYS=20
+# LOG=$LOG_PATH/candidates-clean-manga-$(date "+%Y-%m-%d-%T").log
+# kmanga/manage.py clean manga --list -d $DAYS &> $LOG
+# bin/check-log.sh $LOG
 
-DAYS=30
-LOG=$LOG_PATH/clean-manga-$(date "+%Y-%m-%d-%T").log
-kmanga/manage.py clean manga --force -d $DAYS &> $LOG
-bin/check-log.sh $LOG
+# DAYS=30
+# LOG=$LOG_PATH/clean-manga-$(date "+%Y-%m-%d-%T").log
+# kmanga/manage.py clean manga --force -d $DAYS &> $LOG
+# bin/check-log.sh $LOG
 
 # Clean covers
-LOG=$LOG_PATH/clean-cover-$(date "+%Y-%m-%d-%T").log
-kmanga/manage.py clean cover --force &> $LOG
-bin/check-log.sh $LOG
+# LOG=$LOG_PATH/clean-cover-$(date "+%Y-%m-%d-%T").log
+# kmanga/manage.py clean cover --force &> $LOG
+# bin/check-log.sh $LOG
 
 # Clean cache of images
-DAYS=15
+DAYS=5
 LOG=$LOG_PATH/clean-image-cache-$(date "+%Y-%m-%d-%T").log
 kmanga/manage.py clean image-cache --force -d $DAYS &> $LOG
 bin/check-log.sh $LOG
 
+# Clean cache of issues
+DAYS=5
+LOG=$LOG_PATH/clean-issue-cache-$(date "+%Y-%m-%d-%T").log
+kmanga/manage.py clean issue-cache --force -d $DAYS &> $LOG
+bin/check-log.sh $LOG
+
 # Clean cache of MOBI
-DAYS=15
+DAYS=5
 LOG=$LOG_PATH/clean-mobi-cache-$(date "+%Y-%m-%d-%T").log
 kmanga/manage.py clean mobi-cache --force -d $DAYS &> $LOG
 bin/check-log.sh $LOG
