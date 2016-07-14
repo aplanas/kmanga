41f462588c2e55c1ae5afa802fda7a2f90d8f19e
Alberto Planas <aplanas@gmail.com>
2016-07-14 21:38:25 +0200

    core: improve admin interface.

--- kmanga/core/admin.py
+++ kmanga/core/admin.py
@@ -20,7 +20,8 @@ class GenreInline(admin.StackedInline):
 
 @admin.register(Source)
 class SourceAdmin(admin.ModelAdmin):
-    inlines = [SourceLanguageInline, GenreInline]
+    list_display = ('name', 'spider', 'url')
+    inlines = (SourceLanguageInline, GenreInline)
 
 
 # @admin.register(ConsolidateGenre)
@@ -28,20 +29,38 @@ class SourceAdmin(admin.ModelAdmin):
 #     pass
 
 
-class IssueAdmin(admin.StackedInline):
+class IssueInline(admin.StackedInline):
     model = Issue
 
 
 @admin.register(Manga)
 class MangaAdmin(admin.ModelAdmin):
-    inlines = [IssueAdmin]
+    list_display = ('name', 'status', 'source', 'url')
+    list_filter = ('source',)
+    inlines = (IssueInline,)
 
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('manga', 'source', 'user', 'issues_per_day',
+                    'language', 'paused', 'modified')
+    raw_id_fields = ('manga', 'user')
+    date_hierarchy = 'modified'
+
+    def source(self, subscription):
+        return subscription.manga.source
+    source.short_description = 'Source'
+    source.admin_order_field = 'manga__source'
 
 
 @admin.register(Result)
 class ResultAdmin(admin.ModelAdmin):
+    list_display = ('issue', 'subscription', 'user', 'status', 'created')
+    list_filter = ('status',)
+    raw_id_fields = ('issue', 'subscription')
     date_hierarchy = 'modified'
+
+    def user(self, result):
+        return result.subscription.user
+    user.short_description = 'User'
+    user.admin_order_field = 'subscription__user'
