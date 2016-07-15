0e19e78639479e9cb0c79da4473683bda7c0e88b
Alberto Planas <aplanas@gmail.com>
2016-07-15 19:11:24 +0200

    FTS: add source into the document.

--- bin/0002_full_text_search.py
+++ bin/0002_full_text_search.py
@@ -14,16 +14,21 @@ class Migration(migrations.Migration):
         migrations.RunSQL(
             sql='''
 CREATE MATERIALIZED VIEW core_manga_fts_view AS
-SELECT core_manga.id,
-       core_manga.name,
-       core_manga.url,
-       setweight(to_tsvector(core_manga.name), 'A') ||
-       to_tsvector(core_manga.description) ||
-       to_tsvector(coalesce(string_agg(core_altname.name, ' '),
-                            '')) AS document
-FROM core_manga
-LEFT JOIN core_altname ON core_manga.id = core_altname.manga_id
-GROUP BY core_manga.id;
+                  SELECT core_manga.id,
+                         core_manga.name,
+                         core_manga.url,
+                         setweight(to_tsvector(core_manga.name), 'A') ||
+                         to_tsvector(core_source.name) ||
+                         to_tsvector(core_source.spider) ||
+                         to_tsvector(core_manga.description) ||
+                         to_tsvector(
+                           coalesce(string_agg(core_altname.name, ' '), '')
+                         ) AS document
+                    FROM core_manga
+               LEFT JOIN core_altname ON core_manga.id = core_altname.manga_id
+              INNER JOIN core_source ON core_manga.source_id = core_source.id
+                GROUP BY core_manga.id,
+                         core_source.id;
 
 CREATE INDEX core_manga_fts_idx ON core_manga_fts_view USING gin(document);
 ''',
