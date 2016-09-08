499c6495ac03e1273c38b888e28ca2182be86a6c
Alberto Planas <aplanas@gmail.com>
2016-09-08 22:23:02 +0200

    Source: Add `has_footer` field.

--- kmanga/core/models.py
+++ kmanga/core/models.py
@@ -26,6 +26,7 @@ class Source(TimeStampedModel):
     name = models.CharField(max_length=200)
     spider = models.CharField(max_length=80)
     url = models.URLField(unique=True)
+    has_footer = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
