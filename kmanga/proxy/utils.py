3976be4545f653fdc3af159221df99a62ea5f33d
Alberto Planas <aplanas@gmail.com>
2016-10-26 14:46:04 +0200

    proxy: clean proxy list and add new sources.

--- kmanga/proxy/utils.py
+++ kmanga/proxy/utils.py
@@ -34,36 +34,53 @@ PROXY_MAP = {
     },
 }
 
-# List of URLs that contains proxy lists
+# List of URLs that contains proxy lists and the decoder
 PROXY_SOURCE = [
-    'http://www.httptunnel.ge/ProxyListForFree.aspx',
-    'http://socks24.ru/proxy/httpProxies.txt',
-    'http://txt.proxyspy.net/proxy.txt',
-    'http://www.therealist.ru/proksi/spisok-elitnyx-proksi-serverov',
-    'http://uks.pl.ua/script/getproxy.php?last',
-    'http://proxytime.ru/http',
-    'http://marcosbl.com/lab/proxies/',
-    'http://aliveproxies.com/pages/page-scrapebox-proxies/',
-    'http://www.cybersyndrome.net/pla.html?guid=ON',
-    'http://www.x-scripts.com/proxy.php',
-    'http://www.shroomery.org/ythan/proxylist.php',
-    'http://www.my-proxy.com/free-proxy-list.html',
-    'http://www.my-proxy.com/free-proxy-list-2.html',
-    'http://www.my-proxy.com/free-proxy-list-3.html',
-    'http://www.my-proxy.com/free-proxy-list-4.html',
-    'http://www.my-proxy.com/free-proxy-list-5.html',
-    'http://www.my-proxy.com/free-proxy-list-6.html',
-    'http://www.my-proxy.com/free-proxy-list-7.html',
-    'http://www.my-proxy.com/free-proxy-list-8.html',
-    'http://www.my-proxy.com/free-proxy-list-9.html',
-    'http://www.my-proxy.com/free-proxy-list-10.html',
-    'http://proxy-ip-list.com/',
+    ('http://www.httptunnel.ge/ProxyListForFree.aspx', 'plain'),
+    ('http://txt.proxyspy.net/proxy.txt', 'plain'),
+    ('http://proxytime.ru/http', 'plain'),
+    ('http://marcosbl.com/lab/proxies/', 'plain'),
+    ('http://aliveproxies.com/pages/page-scrapebox-proxies/', 'plain'),
+    ('http://www.cybersyndrome.net/pla.html?guid=ON', 'plain'),
+    ('http://www.my-proxy.com/free-proxy-list.html', 'plain'),
+    ('http://www.my-proxy.com/free-proxy-list-2.html', 'plain'),
+    ('http://www.my-proxy.com/free-proxy-list-3.html', 'plain'),
+    ('http://www.my-proxy.com/free-proxy-list-4.html', 'plain'),
+    ('http://www.my-proxy.com/free-proxy-list-5.html', 'plain'),
+    ('http://www.my-proxy.com/free-proxy-list-6.html', 'plain'),
+    ('http://www.my-proxy.com/free-proxy-list-7.html', 'plain'),
+    ('http://www.my-proxy.com/free-proxy-list-8.html', 'plain'),
+    ('http://www.my-proxy.com/free-proxy-list-9.html', 'plain'),
+    ('http://www.my-proxy.com/free-proxy-list-10.html', 'plain'),
+    ('http://proxy-ip-list.com/', 'plain'),
+    ('http://proxy-ip-list.com/free-usa-proxy-ip.html', 'plain'),
+    ('http://proxy-ip-list.com/free-uk-proxy-list.html', 'plain'),
+    ('http://proxy-ip-list.com/proxy-list-port-3128.html', 'plain'),
+    ('http://proxy-ip-list.com/proxy-8080.html', 'plain'),
+    ('http://proxy-ip-list.com/fresh-proxy-list.html', 'plain'),
+    ('http://proxy-ip-list.com/download/free-proxy-list.txt', 'plain'),
+    ('http://proxy-ip-list.com/download/proxy-list-port-3128.txt', 'plain'),
+    ('http://proxy-ip-list.com/download/free-usa-proxy-ip.txt', 'plain'),
+    ('http://proxy-ip-list.com/download/free-uk-proxy-list.txt', 'plain'),
+    ('http://www.proxylists.net/proxylists.xml', 'xml'),
+    ('http://www.proxyrss.com/proxylists/all.gz', 'gz'),
+    ('http://www.proxz.com/proxylists.xml', 'xml'),
 ]
 
 
 def update_proxy():
     """Collect new proxies and return validation array."""
-    proxies = _collect_proxies()
+    collector = {
+        'plain': _collect_proxies_plain,
+        'xml': _collect_proxies_xml,
+        'gz': _collect_proxies_gz,
+    }
+
+    proxies = []
+    for url, kind in PROXY_SOURCE:
+        proxies.extend(collector[kind](url))
+    # Remove duplicates and localhost references
+    proxies = list(set(proxies)-set(('127.0.0.1',)))
     return check_proxy(proxies)
 
 
@@ -79,18 +96,44 @@ def needs_proxy(spider):
     return spider in PROXY_MAP
 
 
-def _collect_proxies():
+def _get_url(url):
+    """Get the content from a URL."""
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:49.0) '\
+                 'Gecko/20100101 Firefox/49.0'
+    request = urllib2.Request(url)
+    request.add_header('User-Agent', user_agent)
+    body = ''
+    try:
+        body = urllib2.urlopen(request).read()
+    except Exception:
+        logger.warning('Fail URL %s' % url)
+    return body
+
+
+def _collect_proxies_plain(url):
     """Generate a list of proxies from different sources."""
+    logger.info('Collecting proxies (plain) from %s' % url)
     proxy_re = re.compile(r'((?:\d{1,3}.){3}\d{1,3}:\d{1,4})')
-    proxies = []
-    for url in PROXY_SOURCE:
-        logger.info('Collecting proxies from %s' % url)
-        try:
-            body = urllib2.urlopen(url).read()
-            proxies.extend(proxy_re.findall(body))
-        except Exception:
-            logger.warning('Fail URL %s' % url)
-    return list(set(proxies)-set(('127.0.0.1',)))
+    return proxy_re.findall(_get_url(url))
+
+
+def _collect_proxies_xml(url):
+    """Generate a lost of proxies from XML."""
+    logger.info('Collecting proxies (XML) from %s' % url)
+    proxy_re = re.compile(
+        r'<prx:ip>((?:\d{1,3}.){3}\d{1,3})</prx:ip>'
+        r'<prx:port>(\d{1,4})</prx:port>')
+    body = _get_url(url)
+    return ['%s:%s' % (ip, port) for ip, port in proxy_re.findall(body)]
+
+
+def _collect_proxies_gz(url):
+    """Generate a lost of proxies from gzip file."""
+    logger.info('Collecting proxies (gzip) from %s' % url)
+    body = StringIO.StringIO(_get_url(url))
+    body = gzip.GzipFile(fileobj=body).read()
+    proxy_re = re.compile(r'((?:\d{1,3}.){3}\d{1,3}:\d{1,4})')
+    return proxy_re.findall(body)
 
 
 def _is_valid_proxy(proxy_source):
