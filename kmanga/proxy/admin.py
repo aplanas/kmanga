a8f3578a59f6331f4d9731e838e4c5870048a1e0
Alberto Planas <aplanas@gmail.com>
2016-10-26 21:30:19 +0200

    proxy: add `retry` field and helpers.

--- kmanga/proxy/admin.py
+++ kmanga/proxy/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 from .models import Proxy
 
 
-admin.site.register(Proxy)
+@admin.register(Proxy)
+class ProxyAdmin(admin.ModelAdmin):
+    list_display = ('proxy', 'source', 'retry', 'created')
--- kmanga/proxy/models.py
+++ kmanga/proxy/models.py
@@ -7,13 +7,45 @@ from core.models import Source
 from core.models import TimeStampedModel
 
 
+class ProxyQuerySet(models.QuerySet):
+    def get_one(self, spider):
+        """Get one proxy for a specific source."""
+        return self.filter(source__spider=spider).order_by('?').first()
+
+    def discard(self, proxy, spider):
+        """Discard a proxy that is failing."""
+        try:
+            proxy = self.get(proxy=proxy, source__spider=spider)
+            proxy.discard()
+        except Proxy.DoesNotExist:
+            pass
+
+    def remainings(self, spider):
+        """Return the number of proxy for a source."""
+        return self.filter(source__spider=spider).count()
+
+
 @python_2_unicode_compatible
 class Proxy(TimeStampedModel):
+    # Number of retries before giving up and removing the entry
+    RETRY = 3
+
     proxy = models.CharField(max_length=32)
     source = models.ForeignKey(Source)
+    retry = models.IntegerField(default=0)
+
+    objects = ProxyQuerySet.as_manager()
 
     class Meta:
         unique_together = ('proxy', 'source')
 
     def __str__(self):
         return '%s (%s)' % (self.proxy, self.source.name)
+
+    def discard(self):
+        """Discard a proxy that is failing."""
+        self.retry += 1
+        if self.retry <= Proxy.RETRY:
+            self.save()
+        else:
+            self.delete()
