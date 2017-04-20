57402fcb8fc8a2b1aa0496fbc2021bc218f0ad57
Alberto Planas <aplanas@gmail.com>
2017-04-20 07:05:26 +0200

    Add VPN-Gate alternative

---
+++ bin/set-vpn-gate.sh
@@ -0,0 +1,115 @@
+#!/bin/sh
+
+set -e
+
+# Load the configuration file if exist
+KMANGA_CONF=$(dirname $0)/kmanga.conf
+if [ ! -f "$KMANGA_CONF" ]; then
+    echo "Please, create a $KMANGA_CONF configuration file."
+    exit 1
+fi
+source $KMANGA_CONF
+
+UNPACK=$(dirname $0)/vpn-gate-unpack.py
+
+
+# Shows usage information
+function usage() {
+    echo "Usage: $0 <string>" 1>&2
+    echo 1>&2
+    echo "Where <string> is the host name used in the configuration file" 1>&2
+    exit 1
+}
+
+
+# Read command line parameters
+if [ $# -eq 0 ]; then
+    usage
+fi
+# Base directory for the configuration files for hosts
+HOST_CONF_PATH=$(dirname $0)/vpn.d
+# Config file for the host name
+HOST_CONF=$HOST_CONF_PATH/vpn_for_$1.conf
+
+# Sourcing the configuration file will populate the variables: HOST,
+# COMMAND, RENEW_MIN, RENEW_MAX
+source $HOST_CONF
+
+# List of IPs for the host
+IPADDRS=$(getent hosts $HOST | awk '{ print $1 }')
+
+# We append $HOST to $VPN_PATH, that was defined in kmanga.conf
+VPN_PATH=$VPN_PATH/$HOST
+
+OVPN_CONF="non-valid-configuration"
+
+
+# Create the configuration directory and download the ovpn
+function download_ovpn() {
+    # If the new directory exist, recreate it
+    if [ -d "$VPN_PATH.new" ]; then
+	rm -fr "$VPN_PATH.new"
+    fi
+    mkdir "$VPN_PATH.new"
+
+    # Download the new configuration
+    pushd "$VPN_PATH.new"
+    echo "Unpacking vpn-gate configuration..."
+    python ../$UNPACK --host $HOST
+    popd
+
+    # Interchange the configuration directory
+    if [ -d "$VPN_PATH" ]; then
+	rm -fr "$VPN_PATH"
+    fi
+    mv "$VPN_PATH.new" "$VPN_PATH"
+    echo -e "vpn\nvpn" > "$VPN_PATH/auth.txt"
+}
+
+
+function rnd_sleep() {
+    local minutes=$(($RANDOM % $RENEW_MAX))
+    minutes=$(($minutes < $RENEW_MIN ? $RENEW_MIN : $minutes))
+    echo "Renew in $minutes minutes..."
+    sleep $(($minutes * 60))
+}
+
+
+function run_openvpn() {
+    local tries=30;
+    for conf in $(ls $VPN_PATH/*.ovpn | sort --random-sort); do
+	tries=$((tries-1))
+	if [[ $tries -le 0 ]]; then
+	    echo "No more tries, restarting the configuration"
+	    return
+	fi
+
+	local proc="openvpn --config $OVPN_CONF"
+	if pgrep --full "$proc" >/dev/null; then
+	    echo "Closing $OVPN_CONF connection..."
+	    pkill --full "$proc"
+	    sleep 2
+	fi
+	OVPN_CONF=$conf
+	echo "Starting $OVPN_CONF connection..."
+	pushd $VPN_PATH
+	openvpn --config "$OVPN_CONF" &
+	popd
+	sleep 5
+
+	# Check if the VPN is working
+	if sudo -u $AS_USER -- sh -c "$COMMAND"; then
+	    echo "VPN valid found!"
+	    rnd_sleep
+	else
+    	    echo "VPN not valid! - reconnecting"
+	fi
+    done
+}
+
+
+# Main loop
+while /bin/true; do
+    download_ovpn
+    run_openvpn
+done
---
+++ bin/vpn-gate-unpack.py
@@ -0,0 +1,47 @@
+import argparse
+import base64
+import csv
+import tempfile
+import urllib
+
+# List of URLs that contains openvpn configurations
+URLS = (
+    # (URL, skip_lines)
+    ('http://www.vpngate.net/api/iphone/', 2),
+    ('http://easyvpn.rusweb.club/?type=csv', 0),
+)
+
+NAME_INDEX = 0
+OVPN_INDEX = 14
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Unpack vpn-gate files')
+    parser.add_argument('--host', required=True, help='route host for openvpn')
+    args = parser.parse_args()
+
+    cfg = '''
+auth-user-pass auth.txt
+route-nopull
+route %s
+''' % args.host
+
+    for url, skip_lines in URLS:
+        # Use a temporary file to bypass the CSV module limitation for
+        # files with EOL based on Mac
+        with tempfile.TemporaryFile(mode='r+bU') as tmpfile:
+            tmpfile.write(urllib.urlopen(url).read())
+            # Position the cursor to the first data line
+            tmpfile.seek(0)
+            for _ in range(skip_lines):
+                tmpfile.readline()
+
+            reader = csv.reader(tmpfile)
+            for i, row in enumerate(reader):
+                try:
+                    name = '%s-%03d.ovpn' % (row[NAME_INDEX], i)
+                    ovpn = base64.b64decode(row[OVPN_INDEX]) + cfg
+                    open(name, 'w').write(ovpn)
+                except IndexError:
+                    # In some cases the last line is not valid
+                    pass
