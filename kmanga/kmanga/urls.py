61162f09e8c61fa22831aea101b2899a28bf01fc
Alberto Planas <aplanas@gmail.com>
2018-08-24 15:57:08 +0200

    WIP: Migration to Django 2.1

--- kmanga/kmanga/urls.py
+++ kmanga/kmanga/urls.py
@@ -37,10 +37,10 @@ urlpatterns = [
 ]
 
 # Remove this in deployment
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
 from django.conf import settings
 from django.conf.urls.static import static
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     try:
